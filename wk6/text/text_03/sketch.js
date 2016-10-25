a1d6537b00b5adb4524bc4404c4831892d30c2ed
LadyK <static.katherine@gmail.com>
2016-10-24 21:04:16 -0400

    more demos from class with font

--- wk6/text/text_03/sketch.js
+++ wk6/text/text_03/sketch.js
@@ -23,6 +23,26 @@ function setup() {
 
 function draw() {
   background(255);
+  var thex = 50; 
+  var they = height/2;
+  textFont(thefont[0], 36);
+  text('my', thex, they);
+  thex += textWidth('my ');
+  
+  textFont(thefont[1], 36);
+  text('cat' ,thex, they);
+  thex += textWidth('cat ');
+  
+  textFont(thefont[2], 36);
+  text('has' ,thex, they);
+  thex += textWidth('has '); // with regard to the text you just drew
+  
+  textFont(thefont[3], 36);
+  text('ticks' ,thex, they);
+  thex += textWidth('ticks ');// totally optional
+  
+ 
+  /*
   fill(insideColor, 0, 0);
   text('my cat has ticks', mouseX, mouseY);
   fsize++;
@@ -35,4 +55,6 @@ function draw() {
     fontCount = (fontCount + 1) % thefont.length;
     textFont(thefont[fontCount]);
   }
+  
+  */
 }
---
+++ wk6/text/text_04/sketch.js
@@ -0,0 +1,85 @@
+var thefont = new Array(4);
+var fphases = new Array(4);
+var testphase = 0;
+var speed = 0.01;
+
+
+var fsize = 9;
+var insideColor = 0;
+var fontCount = 0;
+
+
+function preload() {
+
+  //thefont = loadFont('data/font1.otf');
+  for (var i = 0; i < thefont.length; i++) {
+    thefont[i] = loadFont('data/font' + i + '.otf'); // concatenating the name of the file
+  }
+}
+
+function setup() {
+  createCanvas(800, 600);
+  background(255);
+  fill(0);
+  stroke(0);
+  textFont(thefont[0], fsize); // which font and the size
+  
+  
+  for(var i = 0; i < fphases.length; i++){
+    fphases[i] = random(0.0, TWO_PI);
+  }
+
+}
+
+function draw() {
+  background(255);
+  var thex = 50; 
+  var they = height/2;
+  
+  speed = mouseX/width * 0.5;
+  testphase = (testphase + speed) % TWO_PI;
+  var h = map(sin(testphase), -1.0, 1.0, height, 0);
+  ellipse(width/2, h, 20, 20);
+  
+  var s;
+  
+  s = map(sin(fphases[0]), -1.0, 1.0, 9, 64);
+  textFont(thefont[0], s);
+  text('my', thex, they);
+  thex += textWidth('my ');
+  fphases[0] = (fphases[0] + speed) % TWO_PI;
+  
+  s = map(sin(fphases[1]), -1.0, 1.0, 9, 64);
+  textFont(thefont[1], s);
+  text('cat' ,thex, they);
+  thex += textWidth('cat ');
+  fphases[1] = (fphases[1] + speed) % TWO_PI;
+  
+  s = map(sin(fphases[2]), -1.0, 1.0, 9, 64);
+  textFont(thefont[2], s);
+  text('has' ,thex, they);
+  thex += textWidth('has '); // with regard to the text you just drew
+  fphases[2] = (fphases[2] + speed) % TWO_PI;
+  
+  s = map(sin(fphases[3]), -1.0, 1.0, 9, 64);
+  textFont(thefont[3], s);
+  text('ticks' ,thex, they);
+  thex += textWidth('ticks ');// totally optional
+  fphases[3] = (fphases[3] + speed) % TWO_PI;
+ 
+  /*
+  fill(insideColor, 0, 0);
+  text('my cat has ticks', mouseX, mouseY);
+  fsize++;
+  textSize(fsize);
+
+  if (fsize > 56) fsize = 9;
+
+  if (frameCount % 20 === 0) { // new in p5 --> 3x= when comparing to zero
+    insideColor = 255 - insideColor;
+    fontCount = (fontCount + 1) % thefont.length;
+    textFont(thefont[fontCount]);
+  }
+  
+  */
+}

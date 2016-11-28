40b88d70be4677a7c8629cf76433111a76da9471
LadyK <static.katherine@gmail.com>
2016-11-28 18:16:43 -0500

    pushing up examples for class beforehand

---
+++ wk12/flappyBirds_01/bird.js
@@ -0,0 +1,43 @@
+function Bird(){
+  this.y = height/2;
+  this.x = 64;
+  
+  this.gravity = 1; // force causing the velocity to increase. down
+  //but better with other forces:
+  this.lift = -10; // negative added so goes down
+  this.velocity = 0;
+  
+  this.show = function(){
+    fill(255);
+    ellipse(this.x, this.y, 32, 32);
+  }
+  
+  
+  this.up = function(){
+    this.velocity += this.lift
+    this.velocity *= 0.9;  // air resistance limits speed. shrinking it a bit
+    this.velocity += this.gravity * 10;
+    
+  }
+  this.update = function(){
+    this.velocity += this.gravity;
+    this.y += this.velocity;  // pushing bird down
+  
+    
+    if(this.y > height){
+      this.y = height;
+      this.velocity = 0;
+    }
+    
+    // also for the top
+    if(this.y < 0){
+      this.y = 0;
+      this.velocity = 0;
+    }
+    
+    
+    
+    
+    
+  }
+}
---
+++ wk12/flappyBirds_01/block.js
@@ -0,0 +1,31 @@
+function Block() {
+
+  this.top = random(height / 2);
+  this.bottom = random(height / 2);
+  this.x = width;
+  this.w = 20;
+  this.speed = 2;
+  
+  
+
+  this.show = function() {
+    fill(255);
+  
+    
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, height - this.bottom, this.w, this.bottom);
+  }
+
+  this.update = function() {
+    this.x -= this.speed;
+  }
+
+  this.offscreen = function() {
+    if (this.x < -this.w) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
